f12599f07984f0d490669e6ec83a4566c4c1d4a7
WindowsS <Asimodabas03@hotmail.com>
2023-05-21 17:50:47 +0300

    flow activity_flow.xml create

---
+++ app/src/main/java/com/asimodabas/deepfake_video_detection_app/ui/activity/FlowActivity.kt
@@ -0,0 +1,12 @@
+package com.asimodabas.deepfake_video_detection_app.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.asimodabas.deepfake_video_detection_app.R
+
+class FlowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flow)
+    }
+}

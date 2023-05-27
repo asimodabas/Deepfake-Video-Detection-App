ac6df32e22cf1bab54a71eb5e5577768ee7eccb4
WindowsS <Asimodabas03@hotmail.com>
2023-05-27 21:21:42 +0300

    gradle updated, home fragment viewmodel menus added

---
+++ app/src/main/java/com/asimodabas/deepfake_video_detection_app/ui/fragment/home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.asimodabas.deepfake_video_detection_app.ui.fragment.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.asimodabas.deepfake_video_detection_app.databinding.FragmentHomeBinding
+import com.asimodabas.deepfake_video_detection_app.util.viewBinding
+
+class HomeFragment : Fragment() {
+
+    private val binding by viewBinding(FragmentHomeBinding::bind)
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
---
+++ app/src/main/java/com/asimodabas/deepfake_video_detection_app/ui/fragment/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.asimodabas.deepfake_video_detection_app.ui.fragment.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}

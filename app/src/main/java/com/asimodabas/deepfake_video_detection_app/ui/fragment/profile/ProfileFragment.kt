a2d96477f034a1b57cf2af08cbe3fcb3cc7459ee
WindowsS <Asimodabas03@hotmail.com>
2023-05-29 22:20:08 +0300

    profile fragment, viewmodel,xml added

--- app/src/main/java/com/asimodabas/deepfake_video_detection_app/ui/fragment/profile/ProfileFragment.kt
+++ app/src/main/java/com/asimodabas/deepfake_video_detection_app/ui/fragment/profile/ProfileFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.asimodabas.deepfake_video_detection_app.R
 import com.asimodabas.deepfake_video_detection_app.databinding.FragmentProfileBinding
+import com.asimodabas.deepfake_video_detection_app.util.toastMessage
 import com.asimodabas.deepfake_video_detection_app.util.viewBinding
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     private val binding by viewBinding(FragmentProfileBinding::bind)
     private val viewModel: ProfileViewModel by viewModels()
@@ -15,5 +18,23 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        getProfileInfo()
+
+        binding.EditProfileTextView.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_editFragment)
+        }
+    }
+
+    private fun getProfileInfo() {
+        viewModel.pullUserInfo().observe(viewLifecycleOwner) { dataConfirm ->
+            dataConfirm?.let {
+                binding.userModel = dataConfirm
+            }
+        }
+        viewModel.errorMessage.observe(viewLifecycleOwner) { error ->
+            error?.let {
+                toastMessage(requireContext(), it)
+            }
+        }
     }
 }
--- app/src/main/java/com/asimodabas/deepfake_video_detection_app/ui/fragment/profile/ProfileViewModel.kt
+++ app/src/main/java/com/asimodabas/deepfake_video_detection_app/ui/fragment/profile/ProfileViewModel.kt
@@ -1,6 +1,35 @@
 package com.asimodabas.deepfake_video_detection_app.ui.fragment.profile
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.asimodabas.deepfake_video_detection_app.model.UserModel
+import com.asimodabas.deepfake_video_detection_app.util.Constants.USERS
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class ProfileViewModel : ViewModel() {
+
+    val errorMessage = MutableLiveData<String>()
+    private val getUuid = FirebaseAuth.getInstance().uid.toString()
+    private var liveData: MutableLiveData<UserModel>? = null
+    private lateinit var databaseReference: DatabaseReference
+
+    fun pullUserInfo(): LiveData<UserModel> {
+        if (liveData == null) liveData = MutableLiveData()
+        databaseReference =
+            FirebaseDatabase.getInstance().getReference(USERS).child(getUuid)
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    val userModel = snapshot.getValue(UserModel::class.java)
+                    liveData!!.postValue(userModel)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
+        return liveData!!
+    }
 }
